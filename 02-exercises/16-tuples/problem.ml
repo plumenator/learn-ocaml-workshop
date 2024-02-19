b5e70dc9ab7d08d024a910d61e2dfb8e5b3dc83e
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2024-02-20 02:08:37 +0800

    Solve 16-tuples

--- 02-exercises/16-tuples/problem.ml
+++ 02-exercises/16-tuples/problem.ml
@@ -21,7 +21,8 @@ let () =
    Write a function that computes the sum of two coordinates. *)
 type coordinate = int * int
 
-let add coord1 coord2 = failwith "For you to implement"
+let add coord1 coord2 =
+  match (coord1, coord2) with (x1, y1), (x2, y2) -> (x1 + x2, y1 + y2)
 
 (* Now consider a [name] type containing two [string]s representing the first and
    the last name. *)
@@ -55,8 +56,8 @@ let nested_char_pair : char pair pair = (('a', 'b'), ('c', 'd'))
 
 (* Write functions to extract the first and second elements from a pair. Their
    signatures can be found in the mli. *)
-let first pair = failwith "For you to implement"
-let second pair = failwith "For you to implement"
+let first pair = match pair with f, _ -> f
+let second pair = match pair with _, s -> s
 
 (* Notice the cool [%compare.equal: int * int] here!  *)
 let%test "Testing add..." =
