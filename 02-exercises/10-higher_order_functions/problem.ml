ddcd190c8769f3de013470f2d9c4b17c57c768d9
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2024-02-19 20:32:29 +0800

    Reformat

--- 02-exercises/10-higher_order_functions/problem.ml
+++ 02-exercises/10-higher_order_functions/problem.ml
@@ -6,20 +6,13 @@ let times x y = x * y
 (* Sometimes, multiple functions look similar. For example, consider
    [add_every_number_up_to] and [factorial]. *)
 let rec add_every_number_up_to x =
-  match x with
-  | 0 -> 0
-  | _ -> plus x (add_every_number_up_to (x - 1))
-;;
+  match x with 0 -> 0 | _ -> plus x (add_every_number_up_to (x - 1))
 
-let rec factorial x =
-  match x with
-  | 0 -> 1
-  | _ -> times x (factorial (x - 1))
-;;
+let rec factorial x = match x with 0 -> 1 | _ -> times x (factorial (x - 1))
 
 (* These functions have a lot in common:
 
-   {| 
+   {|
        let rec NAME x =
        match x with
        | 0 -> ANSWER
@@ -30,10 +23,7 @@ let rec factorial x =
 (* OCaml lets us write the common parts just once. We just have to add some
    extra arguments. *)
 let rec up_to answer combine x =
-  match x with
-  | 0 -> answer
-  | _ -> combine x (up_to answer combine (x - 1))
-;;
+  match x with 0 -> answer | _ -> combine x (up_to answer combine (x - 1))
 
 (* Now we can write our original functions in one line each!
 
@@ -52,36 +42,33 @@ let simpler_factorial x = up_to 1 times x
    [let simpler_add_every_number_up_to x = up_to 0 ( + ) x]
    [let simpler_factorial x = up_to 1 ( * ) x] *)
 
-(* Now let's try refactoring another example. 
+(* Now let's try refactoring another example.
 
    Remember [sum] and [product]? *)
-let rec sum xs =
-  match xs with
-  | [] -> 0
-  | x :: ys -> plus x (sum ys)
-;;
+let rec sum xs = match xs with [] -> 0 | x :: ys -> plus x (sum ys)
+let rec product xs = match xs with [] -> 1 | x :: ys -> times x (product ys)
 
-let rec product xs =
-  match xs with
-  | [] -> 1
-  | x :: ys -> times x (product ys)
-;;
-
-(* These functions look pretty similar too! 
+(* These functions look pretty similar too!
 
    Try factoring out the common parts like we did above. *)
 let rec every answer combine xs = failwith "For you to implement"
 
-(* Can you write a signature in the mli for [every]? How does it compare with [up_to]? 
+(* Can you write a signature in the mli for [every]? How does it compare with [up_to]?
 
    Now, rewrite sum and product in just one line each using [every]. *)
 let simpler_sum xs = failwith "For you to implement"
 let simpler_product xs = failwith "For you to implement"
 
+(* Tests *)
 let%test "Testing simpler_product..." = Int.( = ) 1 (simpler_product [])
 let%test "Testing simpler_product..." = Int.( = ) 55 (simpler_product [ 55 ])
-let%test "Testing simpler_product..." = Int.( = ) 25 (simpler_product [ 5; -5; 1; -1 ])
-let%test "Testing simpler_product..." = Int.( = ) 25 (simpler_product [ 5; 5; 1; 1 ])
+
+let%test "Testing simpler_product..." =
+  Int.( = ) 25 (simpler_product [ 5; -5; 1; -1 ])
+
+let%test "Testing simpler_product..." =
+  Int.( = ) 25 (simpler_product [ 5; 5; 1; 1 ])
+
 let%test "Testing simpler_sum..." = Int.( = ) 0 (simpler_sum [])
 let%test "Testing simpler_sum..." = Int.( = ) 55 (simpler_sum [ 55 ])
 let%test "Testing simpler_sum..." = Int.( = ) 0 (simpler_sum [ 5; -5; 1; -1 ])
