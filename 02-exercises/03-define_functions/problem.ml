6583963d0428f143b8c9da2c3dc724a1cacb1c7d
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2024-02-19 17:15:14 +0800

    Reformat

--- 02-exercises/03-define_functions/problem.ml
+++ 02-exercises/03-define_functions/problem.ml
@@ -4,7 +4,7 @@ open! Base
 
    Definitions take on the form:
 
-   {| let FUNCTION_NAME ARG1 ARG2 ... = BODY |} 
+   {| let FUNCTION_NAME ARG1 ARG2 ... = BODY |}
 
    For example, here we define a function [add1] that takes a single int
    argument and returns that argument plus 1. *)
@@ -21,7 +21,7 @@ let add1_float (arg : float) : float = arg +. 1.
 
    So, you could also write
 
-   {| 
+   {|
        let  FUNCTION_NAME
        ARG1
        ARG2
@@ -38,6 +38,8 @@ let plus x y = x + y
 let times x y = x * y
 let minus x y = x - y
 let divide x y = x / y
+
+(* Tests *)
 
 let%test "Testing plus..." = Int.( = ) 2 (plus 1 1)
 let%test "Testing plus..." = Int.( = ) 49 (plus (-1) 50)
